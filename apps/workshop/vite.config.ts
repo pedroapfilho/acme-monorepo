0d69bd5a2c34d161596ceb33122c4d2eb96aee3a
Pedro Filho <pedro@filho.me>
2023-10-03 07:03:51 -0300

    fixing build

--- apps/workshop/vite.config.ts
+++ apps/workshop/vite.config.ts
@@ -1,7 +1,11 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
