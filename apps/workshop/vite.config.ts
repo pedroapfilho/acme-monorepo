69afbf09cccada1d76296b33bffc173b94196759
Pedro Filho <pedro@filho.me>
2023-10-30 06:22:15 -0300

    fixing deps for tsup

--- apps/workshop/vite.config.ts
+++ apps/workshop/vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-
 import { defineConfig } from "vite";
 
 export default defineConfig({
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig, Options } from "tsup";
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ["src/**/*.tsx"],
+  entry: ["src/index.ts"],
   format: ["esm"],
   dts: true,
   minify: true,
-  clean: true,
+  clean: false,
   external: ["react"],
   ...options,
 }));
