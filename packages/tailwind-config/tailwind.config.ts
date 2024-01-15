fe0a735866ce8cfde4427fd8bca64f9a8864d3de
Pedro Filho <pedro@filho.me>
2024-01-15 12:30:42 -0300

    installing tailwind forms

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -10,36 +10,13 @@ const config: Config = {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-        "blink-caret": {
-          "50%": { background: "transparent" },
-        },
-        "pulse-border": {
-          "50%": { borderColor: "var(--segment-color)" },
-        },
-        shake: {
-          "25%": { transform: "translateX(4px)" },
-          "75%": { transform: "translateX(-4px)" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "blink-caret": "blink-caret 1.2s step-end infinite",
-        "pulse-border": "pulse-border 1s ease-in-out 0s infinite",
-        shake: "shake 0.15s ease-in-out 0s infinite",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+  ],
 };
 
 export default config;
