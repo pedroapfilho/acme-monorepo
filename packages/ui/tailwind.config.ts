10906ac99b4ee72b7ebc4bcd294432b3031be0ce
Pedro Filho <pedro@filho.me>
2024-08-22 09:25:25 -0300

    fixing base styles

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,9 +1,12 @@
 import sharedConfig from "@repo/tailwind-config/tailwind.config.ts";
 import type { Config } from "tailwindcss";
 
-const config: Pick<Config, "prefix" | "presets"> = {
+const config: Pick<Config, "prefix" | "presets" | "corePlugins"> = {
   prefix: "ui-",
   presets: [sharedConfig],
+  corePlugins: {
+    preflight: false,
+  },
 };
 
 export default config;
