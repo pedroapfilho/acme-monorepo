4dd9c3b90453022f8935ccde094a59280c3c8d01
Pedro Filho <pedro@filho.me>
2024-10-04 14:27:19 -0300

    adding lucide to transpiled packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
+  transpilePackages: ["@repo/ui", "lucide-react"],
 };
 
 module.exports = nextConfig;
