a87207874829dcd6cd67eabc65ba59dfee1984e8
Pedro Filho <pedro@filho.me>
2026-06-16 09:52:42 -0300

    chore: trim slop comments (stop-slop sweep) (#130)

--- packages/transactional/src/index.ts
+++ packages/transactional/src/index.ts
@@ -1,13 +1,10 @@
-// Components
 export { AcmeLogo } from "./components/acme-logo";
 export { Button } from "./components/button";
 export { Card } from "./components/card";
 export { Divider } from "./components/divider";
 
-// Utilities
 export { sendEmail, sendBatchEmails, previewEmail } from "./lib/send-email";
 export type { MailerConfig, TransactionalEmail } from "./lib/senders";
 export { sendTransactionalEmail } from "./lib/senders";
 
-// Theme
 export { emailTheme, tailwindConfig } from "./styles/theme";
